b54cce3b574f8419bff031ef5299d88fc31ee588
BalazsiOliver <balazsi.oliver04@gmail.com>
2024-07-01 06:24:46 +0200

    chore: base html of the registration page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form method="post">
+        <p>Welcome to the Pet Clinic!</p>
+        <p>To continue, please do yor registration down below. Thank you for choosing us!</p>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email"></input>
+            <br></br>
+
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username"></input>
+            <br></br>
+
+            <label for="password">Password:</label>
+            <input type="text" id="password" name="password"></input>
+            <br></br>
+                <button type="submit">Submit</button>
+
+        </form>
       </header>
     </div>
   );
