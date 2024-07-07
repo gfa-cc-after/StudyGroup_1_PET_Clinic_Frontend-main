d98f5ab0b2533932d1c488a37d96b8f7605fcc66
Rew <illes.rew.anna@gmail.com>
2024-07-07 19:04:39 +0200

    changed card order, added exotic

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -28,12 +28,18 @@ function LandingPage() {
                 <p>Today's veterinarians are the only doctors educated to protect the health of both animals and people. They work hard to address the health and welfare needs of every species of animal. Veterinarians also play critical roles in environmental protection, research, food safety, and public health.</p>
             </section>
             <section className="services">
+                <div className="sercive-container" id="owner">
+                                                            <h4>For Pet Owners</h4>
+                                                            <p>Follow your pet's medical history.</p>
+                                                            <p>Find the best professionals in one place.</p></div>
+                <div className="sercive-container" id="exotic">
+                                                            <h4>For Exotic Animals</h4>
+                                                            <p>Specialists in exotic pets.</p>
+                                                            <p>Unique care for unique animals.</p></div>
+                <div className="sercive-container"> </div>
                 <div className="sercive-container" id="vet"><h4>For Veterinarians</h4>
                                                             <p>Easy cooperation with other clinics.</p>
                                                             <p>Maintainable, modern patient database.</p></div>
-                <div className="sercive-container"> </div>
-                <div className="sercive-container"> </div>
-                <div className="sercive-container"> </div>
             </section>
         </body>
     )
