e66846f1c130ec52c13d426416a4145600cf8d40
Rew <illes.rew.anna@gmail.com>
2024-07-07 19:13:09 +0200

    added 4th card

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -36,7 +36,11 @@ function LandingPage() {
                                                             <h4>For Exotic Animals</h4>
                                                             <p>Specialists in exotic pets.</p>
                                                             <p>Unique care for unique animals.</p></div>
-                <div className="sercive-container"> </div>
+                <div className="sercive-container" id="wannabeowner"> 
+                                                            <h4>For Wannabe Owners</h4>
+                                                            <p>Find the best pet for you.</p>
+                                                            <p>Learn how to take care of your pet.</p>
+                </div>
                 <div className="sercive-container" id="vet"><h4>For Veterinarians</h4>
                                                             <p>Easy cooperation with other clinics.</p>
                                                             <p>Maintainable, modern patient database.</p></div>
