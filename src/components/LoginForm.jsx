085e148b029a2d353e267066c967c931f9c7ecd7
Ravasz-Vozár Dóra <99547478+Vozard@users.noreply.github.com>
2024-07-10 23:11:32 +0200

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,23 +1,53 @@
-// import '../styles/LoginForm.css';
-import React from 'react'
+import '../styles/LoginForm.css'
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const LOGIN = import.meta.env.VITE_API_BACKEND_URL_LOGIN
 
 const LoginForm = () => {
-  return (
-    <div className="LoginForm">
-      <h1>Login</h1>
-      <form>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} />
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        await axios.post(LOGIN, formData, {
+              headers: { 
+                "Content-Type": "application/json"
+              } 
+        })
+        .then((response) => {console.log('Login successful')})
+        .catch((error) => {console.log('There was an error logging in!', error)})
+    }
+
+    return (
+        <div className="LoginForm">
+            <h1>Login Form</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
+                </div>
+                <button type="submit">Login</button>
+            </form>
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  )
-}
+    );
+
+};
 
 export default LoginForm;
