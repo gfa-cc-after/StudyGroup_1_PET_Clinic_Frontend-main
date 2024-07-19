d45a0ba361ee0d61df1c6808296a1d0237583fcb
Rew <illes.rew.anna@gmail.com>
2024-07-19 16:07:22 +0200

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,10 @@
 import { defineConfig} from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     test:{
         environment: 'jsdom',
         setupFiles: './tests/setup.js',
     },
-
+    plugins: [react()]
 })
