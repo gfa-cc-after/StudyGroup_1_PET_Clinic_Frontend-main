5f8e3f9bf4b8905d1c92bacd33bcb7ff628bc60c
Ravasz-Vozár Dóra <ravaszvozard@gmail.com>
2024-08-04 12:14:35 +0200

    Fix data handling from backend

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,19 +1,31 @@
 import { useState } from 'react';
 import axios from 'axios';
 import '../styles/style.css';
+import { useNavigate } from 'react-router-dom';
 
 const apiUrl = import.meta.env.VITE_API_BACKEND_URL + "/login";
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
 
-    axios.post(apiUrl, { email, password },  {headers: { "Content-Type": "application/json"}})
-    .then((response) => {console.log('Login successful, token: ' + response.data)})
-    .catch((error) => {console.log('There was an error in login.' , error)})
+    try {
+      const response = await axios.post(apiUrl, { email, password },  {headers: { "Content-Type": "application/json"}});
+      
+      localStorage.setItem('token', response.data);
+      navigate('/');
+    } catch (err) {
+      if (!err.response) {
+        setError('There was a network error');
+      } else {
+        setError('There was an error logging in...'+ err.response.data);
+      }
+    }
   }
 
   return (
@@ -43,6 +55,7 @@ const LoginForm = () => {
           />
         </div>
         <button type="submit" className="formButton">Login</button>
+        <p style={{color: 'red'}}>{error ? error : ""}</p>
       </form>
     </div>
   );
