3223e6e4c36804ca43a9ba7cb8e43df61465a63d
markkovari <kovarimarkofficial@gmail.com>
2024-09-13 19:16:30 +0200

    test: add usepet hook values

---
+++ src/hooks/usePets.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import { useAuth } from "./store";
+
+const usePets = () => {
+    const dataUrl = import.meta.env.VITE_API_BACKEND_URL + "/api/v1/user/pets"
+    const { token } = useAuth();
+
+    const [pets, setPets] = useState([]);
+    useEffect(() => {
+
+        const getPets = async () => {
+            const response = await axios.get(
+                dataUrl,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+
+            return response;
+        }
+
+        getPets()
+            .then(response => setPets(response.data.pets))
+            .catch(error => console.error('Error fetching data:', error));
+    }, [0]);
+
+    return { pets, usePets };
+}
+
+export default usePets;
