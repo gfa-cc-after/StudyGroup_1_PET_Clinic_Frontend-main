37afd8a7091b4844d0806a0a5d580761eb30b905
Ravasz-Vozár Dóra <ravaszvozard@gmail.com>
2024-09-13 21:59:00 +0200

    Add additional test lines

--- tests/components/UserHome.test.jsx
+++ tests/components/UserHome.test.jsx
@@ -49,36 +49,42 @@ describe("UserHome component test", () => {
   it("renders all fields correctly", () => {
     // Mock usePets hook to return pet data
     // const { result } = renderHook(() => usePetData());
-
+    
     // Mock implementation of useAuth hook
     const renderedAuthHook = renderHook(() => useAuth())
     
-
+    
     act(() => {
       renderedAuthHook.result.current.setUser('eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoidXNlciIsImRpc3BsYXlOYW1lIjoidGVzdFVzZXIiLCJpZCI6IjIiLCJlbWFpbCI6InRlc3RAdXNlci5jb20iLCJzdWIiOiJ0ZXN0QHVzZXIuY29tIiwiaWF0IjoxNzI1OTE3NTE1LCJleHAiOjE3MjU5MTkzMTV9.GK8y9yFzuNhUzDU9t9csJKCwdrPab-BzP6yyoMfanLI')
-    })
-
+    })    
+    
     const { getByTestId, getAllByTestId } = renderWithRouter(<UserHome />);
+    
+      // Check welcome message
+      const welcomeElement = getByTestId("welcomeId");
+      expect(welcomeElement).toBeInTheDocument();
+      expect(welcomeElement).toHaveTextContent("Welcome testUser!");
+    
+      // expect(result.current.pets).toEqual(mockPetData);
   
-    // Check welcome message
-    const welcomeElement = getByTestId("welcomeId");
-    expect(welcomeElement).toBeInTheDocument();
-    expect(welcomeElement).toHaveTextContent("Welcome testUser!");
-
-    // expect(result.current.pets).toEqual(mockPetData);
-  
-    // Check pet table
-    // const petTable = getByTestId("pet-table");
-    // const petRows = getAllByTestId("pet-row");
-    // expect(petTable).toBeInTheDocument();
-    // expect(petRows.length).toBe(2); // Assuming there are 2 pets in the mock data
-  
-    // Check pet table headers
-    // const petTableHeaders = getAllByTestId("pet-table-header");
-    // expect(petTableHeaders.length).toBe(8); // Assuming there are 8 headers
-  
-    // Check pet table data
-    // const petTableData = getAllByTestId("pet-table-data");
-    // expect(petTableData.length).toBe(16); // Assuming there are 2 pets and 8 headers
+      // Check pet table
+      // const petTable = getByTestId("pet-table");
+      // const petRows = getByTestId("pet-row");
+      // expect(petTable).toBeInTheDocument();
+      // expect(petRows).toBeInTheDocument();
+      // expect(petRows.length).toBe(mockPetData.length)
+      
+      // Check each pet row
+      // mockPetData.forEach((pet, index) => {
+      //   const petRow = petRows[index];
+      //   expect(petRow).toBeInTheDocument();
+      //   expect(petRow).toHaveTextContent(pet.petName);
+      //   expect(petRow).toHaveTextContent(pet.petBreed);
+      //   expect(petRow).toHaveTextContent(pet.petSex);
+      //   expect(petRow).toHaveTextContent(pet.petBirthDate);
+      //   expect(petRow).toHaveTextContent(pet.lastCheckUp);
+      //   expect(petRow).toHaveTextContent(pet.nextCheckUp);
+      //   expect(petRow).toHaveTextContent(pet.specialCondition);
+      // });
   })
 });
