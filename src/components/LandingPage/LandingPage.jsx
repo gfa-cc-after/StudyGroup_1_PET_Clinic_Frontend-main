0c0f00e1aec6bea7306fd0bc1613031c73cd1f24
Márk Kővári <kovarimarkofficial@gmail.com>
2024-09-30 21:02:11 +0200

    chore: add initial landing page outline

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,15 @@
-import '../styles/style.css'
-import logo from '../assets/pet-logo.png'
+import './LandingPage.css'
+import { Grid2, Stack } from '@mui/material'
+import logo from '../../assets/pet-logo.png'
 import { Link } from 'react-router-dom'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 
 function LandingPage() {
     return (
-        <div className="landingbody">
-            <header className="navbar">
+        <Stack direction="column">
+            {/* <header className="navbar">
                 <div id="logo-section">
                     <Link id="logo-link" to="/"><img id="logo-img" src={logo} alt="logo" /></Link>
                     <h1>Pet Clinic Alliance</h1>
@@ -16,37 +20,39 @@ function LandingPage() {
                     <Link className="transparent-button" to="/login" >Log In</Link>
                     <Link className="colored-button" to="/register" >Sign Up</Link>
                 </nav>
-            </header>
+            </header> */}
             <section className="hero">
                 <h2>Your  <span>pet</span>,<wbr /> our <span>passion</span></h2>
                 <h3><span>Free</span> of charge / pain / drama. </h3>
                 <p>Today's veterinarians are the only doctors educated to protect the health of both animals and people. They work hard to address the health and welfare needs of every species of animal. Veterinarians also play critical roles in environmental protection, research, food safety, and public health.</p>
             </section>
-            <section className="services">
-                <div className="service-container" id="owner">
+            <Grid2 className="card" container spacing={4} justifyContent="center">
+                <Card >
                     <h4>For Pet Owners</h4>
                     <p>Follow your pet's medical history.</p>
                     <p>Find the best professionals in one place.</p>
-                </div>
-                <div className="service-container" id="exotic">
+                </Card>
+                <Card>
                     <h4>For Exotic Animals</h4>
                     <p>Specialists in exotic pets.</p>
-                    <p>Unique care for unique animals.</p></div>
-                <div className="service-container" id="wannabeowner">
+                    <p>Unique care for unique animals.</p>
+                </Card>
+                <Card>
                     <h4>For Wannabe Owners</h4>
                     <p>Find the best pet for you.</p>
                     <p>Learn how to take care of your pet.</p>
-                </div>
-                <div className="service-container" id="vet"><h4>For Veterinarians</h4>
+                </Card>
+                <Card>
+                    <h4>For Veterinarians</h4>
                     <p>Easy cooperation with other clinics.</p>
                     <p>Maintainable, modern patient database.</p>
-                </div>
-                <div className="buttons-for-small-devices service-container">
-                    <Link className="transparent-button" to="/login" >Log In</Link>
-                    <Link className="colored-button" to="/register" >Sign Up</Link>
-                </div>
-            </section>
-        </div>
+                </Card>
+            </Grid2>
+            {/* <div className="buttons-for-small-devices service-container">
+                <Link className="transparent-button" to="/login" >Log In</Link>
+                <Link className="colored-button" to="/register" >Sign Up</Link>
+            </div> */}
+        </Stack>
     )
 }
 export default LandingPage
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import LandingPage from './components/LandingPage'
+import LandingPage from './components/LandingPage/LandingPage'
 import LoginForm from './components/LoginForm'
 import RegistrationForm from './components/RegistrationForm'
 import { UserHome } from './components/UserHome'
