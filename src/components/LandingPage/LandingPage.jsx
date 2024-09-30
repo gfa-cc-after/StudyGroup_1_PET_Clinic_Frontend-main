4004eeacc7fcc7a624c059c90356fa8bf6901be3
Márk Kővári <kovarimarkofficial@gmail.com>
2024-09-30 21:11:05 +0200

    feat: add card backrounds on landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,7 +3,6 @@ import { Grid2, Stack } from '@mui/material'
 import logo from '../../assets/pet-logo.png'
 import { Link } from 'react-router-dom'
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 
 
 function LandingPage() {
@@ -27,22 +26,22 @@ function LandingPage() {
                 <p>Today's veterinarians are the only doctors educated to protect the health of both animals and people. They work hard to address the health and welfare needs of every species of animal. Veterinarians also play critical roles in environmental protection, research, food safety, and public health.</p>
             </section>
             <Grid2 className="card" container spacing={4} justifyContent="center">
-                <Card >
+                <Card id="owner">
                     <h4>For Pet Owners</h4>
                     <p>Follow your pet's medical history.</p>
                     <p>Find the best professionals in one place.</p>
                 </Card>
-                <Card>
+                <Card id="exotic">
                     <h4>For Exotic Animals</h4>
                     <p>Specialists in exotic pets.</p>
                     <p>Unique care for unique animals.</p>
                 </Card>
-                <Card>
+                <Card id="wannabeowner">
                     <h4>For Wannabe Owners</h4>
                     <p>Find the best pet for you.</p>
                     <p>Learn how to take care of your pet.</p>
                 </Card>
-                <Card>
+                <Card id="vet">
                     <h4>For Veterinarians</h4>
                     <p>Easy cooperation with other clinics.</p>
                     <p>Maintainable, modern patient database.</p>
